9c823b3ae5a765322ee0ee334e3b8bfa56818674
Jiye <hyeonjiyeah@gmail.com>
2023-07-15 00:07:08 +0900

    fortune model

---
+++ pkg/model/fortune.go
@@ -0,0 +1,11 @@
+package models
+
+import (
+	"time"
+)
+
+type Fortune struct {
+    Ganji      string    `json:"ganji" bson:"ganji" required:"true"`
+    Fortune    string    `json:"fortune" bson:"fortune" required:"true"`
+    CreatedAt  time.Time `json:"createdAt" bson:"createdAt" default:"$currentDate"`
+}
